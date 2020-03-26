b02785945fce92747d9b4978d034397219c735ea
Darckoune <darckoune@gmail.com>
2020-03-26 12:19:44 +0100

    translate bacl cards

--- server/black-cards.ts
+++ server/black-cards.ts
@@ -3,82 +3,82 @@ import { Card } from "./interface/card.model";
 export const blackCards: Card[] = [
     { 
         id: 0,
-        name: "Banana Peel", 
-        effect: "Give one of your equipment cards to another character. If you have none take 1 point of damage" 
+        name: "Peau de banane", 
+        effect: "Donnez un de vos équipements à un autre joueur. Si vous n'en avez pas, subissez 1 blessure." 
     },
     { 
         id: 1,
-        name: "Bloodthirsty Spider", 
-        effect: "Give 2 points of damage to any player's character and take 2 points of damage yourself" 
+        name: "Araignée sanguinaire", 
+        effect: "Infligez 2 blessures au personnage de vottre choix puis subissez vous-même 2 blessures." 
     },
     { 
         id: 2,
-        name: "Butcher Knife", 
-        effect: "If your attack is successful, you give 1 extra point of damage" 
+        name: "Hachoir maudit", 
+        effect: "Si votre attaque inflige des blessures, la victime subit 1 blessure supplémentaire." 
     },
     { 
         id: 3,
-        name: "Chainsaw", 
-        effect: "If your attack is successful, you give 1 extra point of damage" 
+        name: "Tronçonneuse du mal", 
+        effect: "Si votre attaque inflige des blessures, la victime subit 1 blessure supplémentaire." 
     },
     { 
         id: 4,
-        name: "Diabolic Ritual", 
-        effect: "If you are a Shadow you may reveal your identity and fully heal your damage" 
+        name: "Rituel diabolique", 
+        effect: "Si vous êtes shadow, vous pouvez réléver votre identité pour soigner toutes vos blessures." 
     },
     { 
         id: 5,
         name: "Dynamite", 
-        effect: "Roll both dice and give 3 points of damage to all characters in that area. On a 7 nothing happens" 
+        effect: "Lancez les dés et infligez 3 blessures à tous les personnages présents dans le secteur désigné par le total des dés. Il ne se passe rien si le total est 7." 
     },
     { 
         id: 6,
-        name: "Handgun  ", 
-        effect: "All ranges but yours become your attack range (you cannot attack in your normal range)" 
+        name: "Révolver des ténèbres", 
+        effect: "Vous pouvez attaquer sur les 4 lieux hors de votre secteur. Vous ne pouvez plus attaquer dans votre secteur." 
     },
     { 
         id: 7,
-        name: "Machine Gun", 
-        effect: "Your attack will affect all characters in your attack range. Apply the same damage to all." 
+        name: "Mitrailleuse", 
+        effect: "Vos attaquez affectent tous les personnages à votre portée. Effectuez un seul jet de dés et infligez les mêmes blessures pour tous." 
     },
     { 
         id: 8,
         name: "Masamune", 
-        effect: "You MUST attack and you deal D4 damge. You can't fail" 
+        effect: "Vous êtes obligé d'attaquer durant votre tour. Vous attaquez avec le dé 4 uniquement." 
     },
     { 
         id: 9,
-        name: "Moody Goblin", 
-        effect: "Steal an equipment card from any player's character" 
+        name: "Succube tentatrice", 
+        effect: "Volez une carte équipement au joueur de votre choix" 
     },
     { 
         id: 10,
-        name: "Moody Goblin", 
-        effect: "Steal an equipment card from any player's character" 
+        name: "Succube tentatrice", 
+        effect: "Volez une carte équipement au joueur de votre choix" 
     },
     { 
         id: 11,
-        name: "Rusted Broad Axe", 
-        effect: "If your attack is successful, you give 1 extra point of damage" 
+        name: "Hache tueuse", 
+        effect: "Si votre attaque inflige des blessures, la victime subit 1 blessure supplémentaire." 
     },
     { 
         id: 12,
-        name: "Spiritual Doll", 
-        effect: "Pick a player and roll D6 1-4 give their character 3 points of Damage. 5-6 take 3 points of damage" 
+        name: "Poupée démoniaque", 
+        effect: "Désignez un joueur et lancez le dé à 6 faces. 1 à 4 : infligez-lui 3 blessures. 5 à 6 : subissez 3 blessures." 
     },
     { 
         id: 13,
-        name: "Vampire Bat", 
-        effect: "Give 2 points of damage to any player's character and heal 1 point of your own." 
+        name: "Chauve-souris vampire", 
+        effect: "Infligez deux blessures au joueur de votre choix et soignez une de vos blessures." 
     },
     { 
         id: 14,
-        name: "Vampire Bat", 
-        effect: "Give 2 points of damage to any player's character and heal 1 point of your own." 
+        name: "Chauve-souris vampire", 
+        effect: "Infligez deux blessures au joueur de votre choix et soignez une de vos blessures." 
     },
     { 
         id: 15,
-        name: "Vampire Bat", 
-        effect: "Give 2 points of damage to any player's character and heal 1 point of your own." 
+        name: "Chauve-souris vampire", 
+        effect: "Infligez deux blessures au joueur de votre choix et soignez une de vos blessures." 
     }
 ]
