e2589b8c79f1665145b46127531754a540f5d64a
Darckoune <darckoune@gmail.com>
2020-03-27 18:09:42 +0100

    translate green cards

--- server/green-cards.ts
+++ server/green-cards.ts
@@ -3,50 +3,50 @@ import { Card }from "./interface/card.model";
 export const greenCards: Card[] = [
     { 
         id: 0,
-        name: "Aid",  effect: "Hunter heal 1 damage (if you have none take 1 damage)" },
+        name: "Vision divine",  effect: "Hunter - Soigne 1 blessure (si tu n'en as aucune, subis 1 blessure)" },
     { 
         id: 1,
-        name: "Anger",  effect: "Hunter or Shadow - give 1 equipment to current player or take 1 damage" },
+        name: "Vision furtive",  effect: "Hunter ou Shadow - Donne moi 1 carte équipement ou subis 1 blessure" },
     { 
         id: 2,
-        name: "Anger",  effect: "Hunter or Shadow - give 1 equipment to current player or take 1 damage" },
+        name: "Vision furtive",  effect: "Hunter ou Shadow - Donne moi 1 carte équipement ou subis 1 blessure" },
     { 
         id: 3,
-        name: "Blackmail", effect: "Hunter or Neutral - give 1 equipment to current player or take 1 damage" },
+        name: "Vision enivrante", effect: "Hunter ou Neutre - Donne moi 1 carte équipement ou subis 1 blessure" },
     { 
         id:4,
-        name: "Blackmail", effect: "Hunter or Neutral - give 1 equipment to current player or take 1 damage" },
+        name: "Vision enivrante", effect: "Hunter ou Neutre - Donne moi 1 carte équipement ou subis 1 blessure" },
     { 
         id:5,
-        name: "Bully",  effect: "HP less or equal 11 (A,B,C,E,U) take 1 damage" },
+        name: "Vision clairvoyante",  effect: "Si tes points de vie sont inférieurs à 11 (A,B,C,E,M), subis 1 blessure" },
     { 
         id:6,
-        name: "Exorcism",  effect: "Shadow - take 2 damage" },
+        name: "Vision purificatrice",  effect: "Shadow - Subis 2 blessures" },
     { 
         id:7,
-        name: "Greed",  effect: "Neutral or Shadow - give 1 equipment to current player or take 1 damage" },
+        name: "Vision cupide",  effect: "Neutre ou Shadow - Donne moi 1 carte équipement ou subis 1 blessure" },
     { 
         id:8,
-        name: "Greed",  effect: "Neutral or Shadow - give 1 equipment to current player or take 1 damage" },
+        name: "Vision cupide",  effect: "Neutre ou Shadow - Donne moi 1 carte équipement ou subis 1 blessure" },
     { 
         id:9,
-        name: "Huddle",  effect: "Shadow heal 1 damage (if you have none take 1 damage)" },
+        name: "Vision lugubre",  effect: "Shadow - Soigne 1 blessure (si tu n'en as aucune, subis 1 blessure)" },
     { 
         id:10,
-        name: "Nurturance",  effect: "Neutral heal 1 damage (if you have none take 1 damage)" },
+        name: "Vision réconfortante",  effect: "Neutre - Soigne 1 blessure (si tu n'en as aucune, subis 1 blessure)" },
     { 
         id:11,
-        name: "Predicition",  effect: "Show your character card to current player" },
+        name: "Vision suprême",  effect: "Montre moi ta carte personnage" },
     { 
         id:12,
-        name: "Slap",  effect: "Hunter - take 1 damage" },
+        name: "Vision mortifère",  effect: "Hunter - subis 1 blessure" },
     { 
         id:13,
-        name: "Slap",  effect: "Hunter - take 1 damage" },
+        name: "Vision mortifère",  effect: "Hunter - subis 1 blessure" },
     { 
         id:14,
-        name: "Spell",  effect: "Shadow - take 1 damage" },
+        name: "Vision foudroyante",  effect: "Shadow - subis une blessure" },
     { 
         id:15,
-        name: "Tough Lesson",  effect: "HP greater or equal 12 (D,F,G,V,W) take 2 damage" }
+        name: "Vision destructrice",  effect: "Si tes points de vie sont supérieurs à 12 (D,F,G,V,L), subis 2 blessures" }
 ]
